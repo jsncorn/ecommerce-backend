f03d8dd968c8379c451f14cd00224c96b5f6882f
jsncorn <jsncourn@gmail.com>
2022-02-07 19:14:05 -0800

    updated indexjs with belongs to

--- models/index.js
+++ models/index.js
@@ -1,3 +1,22 @@
 const Product = require('./product');
 const Cateogry = require('./category');
 const Tag = require('./tag');
+const Category = require('./category');
+
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+
+Category.hasMany(Product, {
+    foreignKey: 'category_id'
+});
+
+Product.belongsToMany(Tag)
+
+Tag.belongsToMany(Product)
+
+module.exports = {
+    Product, 
+    Category,
+    Tag
+}
