37bb5eda308b4fd18aaaac66a3d2ca4ff2277d4b
jsncorn <jsncourn@gmail.com>
2022-02-07 19:56:33 -0800

    added one item id find route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag} = require('../../models');
+const { Product, Category, Tag, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
     Product.findAll({
@@ -15,27 +15,52 @@ router.get('/', (req, res) => {
             }
         ]
     })
-    .then(productData => res.json(productData))
-    .catch(err => {
-        console.log(err);
-        res.status(500);
-    })
+        .then(productData => res.json(productData))
+        .catch(err => {
+            console.log(err);
+            res.status(500);
+        })
 })
 
 router.get('/:id', (req, res) => {
-    
+    ProductTag.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'product_name', 'price', 'stock'],
+        include: [
+            {
+                model: Category,
+                attributes: ['category_name']
+            },
+            {
+                model: Tag,
+                attributes: ['tag_name']
+            }
+        ]
+    })
+        .then(productData => {
+            if(!productData) {
+                res.status(404);
+            }
+            res.json(productData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500);
+        })
 })
 
 router.post('/', (req, res) => {
-    
+
 })
 
 router.put('/:id', (req, res) => {
-    
+
 })
 
 router.delete('/id', (req, res) => {
-    
+
 })
 
 module.exports = router;
