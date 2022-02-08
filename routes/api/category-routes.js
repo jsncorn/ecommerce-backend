70c9f0e56981928a320606ba9beb28cab9b9bda8
jsncorn <jsncourn@gmail.com>
2022-02-07 19:32:21 -0800

    added finall categories route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,7 +2,22 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
-
+    Category.findAll({
+        include: {
+            model: Product,
+            attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+    })
+    .then(categoryData => {
+        if(!categoryData) {
+            res.status(404);
+        }
+        res.json(categoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500);
+    })
 })
 
 router.get('/:id', (req, res) => {
