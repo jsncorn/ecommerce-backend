04e31283364c9fe71b82174429bd77a43c412dfa
jsncorn <jsncourn@gmail.com>
2022-02-07 19:34:13 -0800

    added category post

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -43,7 +43,19 @@ router.get('/:id', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-
+    Category.create({
+        category_name: req.body.category_name;
+    })
+    .then(categoryData => {
+        if(!categoryData) {
+            res.status(404);
+        }
+        res.json(categoryData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500);
+    })
 })
 
 router.put('/:id', (req, res) => {
