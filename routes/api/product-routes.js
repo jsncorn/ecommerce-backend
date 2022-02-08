ec4e558730cbf9c8a58fa23dcf9c65c1d40fcca1
jsncorn <jsncourn@gmail.com>
2022-02-07 19:51:34 -0800

    added findall route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -2,7 +2,24 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag} = require('../../models');
 
 router.get('/', (req, res) => {
-
+    Product.findAll({
+        attributes: ['id', 'product_name', 'price', 'stock'],
+        include: [
+            {
+                model: Category,
+                attributes: ['category_name']
+            },
+            {
+                model: Tag,
+                attributes: ['tag_name']
+            }
+        ]
+    })
+    .then(productData => res.json(productData))
+    .catch(err => {
+        console.log(err);
+        res.status(500);
+    })
 })
 
 router.get('/:id', (req, res) => {
@@ -20,3 +37,5 @@ router.put('/:id', (req, res) => {
 router.delete('/id', (req, res) => {
     
 })
+
+module.exports = router;
